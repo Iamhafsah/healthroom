7a7957d805d5313e2b3d57eded4e0830171d7477
hafsah <emekomahafsah@gmail.com>
2021-07-19 11:15:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Gatsby and Tailwind CSS project.
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      montserrat: ['Montserrat, sans-serif'],
+      rammetto:['Rammetto One, cursive']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
